1d1a12416f7dc290e072102276be87444dd30272
Hannah Wang <hw1435@nyu.edu>
2017-12-03 23:36:09 -0500

    particle systems almost complete
    still need to make rockHIT, ice beam, thundershock, dynamic punch?, mega drain... and code

---
+++ Assets/Scripts/Camera/moveScript4Testing.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//PLEASE IGNORE EVERY SCRIPT IN THIS FOLDER
+
+//TESTING TRAILING PARTICLES
+public class moveScript4Testing : MonoBehaviour {
+
+	/*
+	public float speed;
+
+	void Update() {
+		float x = Input.GetAxis ("Horizontal") * Time.deltaTime * speed;
+		float z = Input.GetAxis ("Vertical") * Time.deltaTime * 3f;
+
+		transform.Rotate (0, x, 0);
+		transform.Translate (0, 0, z);
+
+		if (Input.GetKeyDown(KeyCode.A)) {
+			//Debug.Log ("hi");
+			//Vector3 pos = new Vector3(0, 30, 0);
+			testParticles.me.PlayParticles (testParticles.slamPart, transform.position, Quaternion.Euler(-90, 0, 0), 3);
+		}
+		if (Input.GetKeyDown(KeyCode.S)) {
+			//Debug.Log ("S");
+			testParticles.me.PlayParticles (testParticles.hitPart, transform.position, Quaternion.identity, 3);
+		}
+	}
+	*/
+
+
+}
---
+++ Assets/Scripts/Camera/tryingStuff.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//IGNORE THIS SCRIPT
+public class tryingStuff : MonoBehaviour {
+	/*
+	public bool top;
+
+	Vector3 startPos;
+	float endPosY;
+
+	void Start() {
+		if (top) {
+			//startPos = new Vector3 (62.7f, 200f, 0f);
+			startPos = transform.position;
+			endPosY = 35.3f;
+		} else {
+			//startPos = new Vector3 (31f, -200f, 0f);
+			startPos = transform.position;
+			endPosY = -36.8f;
+		}
+		transform.position = startPos;
+	}
+
+	void Update() {
+		if (Input.GetKeyDown(KeyCode.B)) {
+			Bite ();
+		}
+	}
+
+	public float speed;
+
+	public void Bite() {
+		if (top) {
+			if (transform.position.y > 35.3f) {
+				transform.position.y -= 0.1f * Time.deltaTime * speed;
+			}
+		} else {
+			if (transform.position.y < -36.8f) {
+				transform.position.y += 0.1f * Time.deltaTime * speed;
+			}
+		}
+	}
+	*/
+}
