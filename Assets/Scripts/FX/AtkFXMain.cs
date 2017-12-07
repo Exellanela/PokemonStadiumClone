81d9ed523e264d8e8aecd4aa1995f6fa28936b55
Hannah Wang <hw1435@nyu.edu>
2017-12-07 12:12:25 -0500

    quick commit

--- Assets/Scripts/FX/AtkFXMain.cs
+++ Assets/Scripts/FX/AtkFXMain.cs
@@ -2,108 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//TO USE THIS SCRIPT (to play particle systems), type the line of code below
+//AtkFXMain.me.PlayParticles(name, location, rotation); //put this line anywhere you want in your code and fill in the variables
+
 public class AtkFXMain : MonoBehaviour {
 
-	/*
-	//general effects for dmg animations and/or normal-type attacks
-	public static ParticleSystem hitPart;
-	public static ParticleSystem slamPart;
-	public static ParticleSystem dustPart;
-	public static ParticleSystem dustTrailPart;
-	public static ParticleSystem recoilPart;
-
-	//accessible PSs -- input into PSname
-	public static ParticleSystem acidPart;
-	public static ParticleSystem emberPart;
-	public static ParticleSystem exploPart;
-	public static ParticleSystem icePart;
-	public static ParticleSystem megaDrainPart;
-	public static ParticleSystem quickAtkPart;
-	public static ParticleSystem rockPart;
-	public static ParticleSystem swiftPart;
-	public static ParticleSystem thunderPart;
-	public static ParticleSystem toxicPart;
-	public static ParticleSystem waterPart;
-
-	//accessible HIT PSs (for dmg) -- input into PSname;
-	public static ParticleSystem acidHit;
-	public static ParticleSystem emberHit;
-	public static ParticleSystem exploHit;
-	public static ParticleSystem iceHit;
-	public static ParticleSystem megaDrainHit;
-	public static ParticleSystem rockHit;
-	public static ParticleSystem swiftHit;
-	public static ParticleSystem thunderHit;
-	public static ParticleSystem toxicHit;
-	public static ParticleSystem waterHit;
-
-	//vars that this code reads to define the above vars
-	public ParticleSystem slam;
-	public ParticleSystem hit;
-	public ParticleSystem dust;
-	public ParticleSystem dustTrail;
-	public ParticleSystem recoil;
-
-	public ParticleSystem acidA;
-	public ParticleSystem emberA;
-	public ParticleSystem exploA;
-	public ParticleSystem drainA;
-	public ParticleSystem iceA;
-	public ParticleSystem quickA;
-	public ParticleSystem rockA;
-	public ParticleSystem swiftA;
-	public ParticleSystem thunderA;
-	public ParticleSystem toxicA;
-	public ParticleSystem wtrA;
-
-	public ParticleSystem acidH;
-	public ParticleSystem emberH;
-	public ParticleSystem exploH;
-	public ParticleSystem drainH;
-	public ParticleSystem iceH;
-	public ParticleSystem rockH;
-	public ParticleSystem swiftH;
-	public ParticleSystem thunderH;
-	public ParticleSystem toxicH;
-	public ParticleSystem wtrH;
-	*/
-
 	public static AtkFXMain me;
-
-	void Start() {
-		/*
-		slamPart = slam;
-		hitPart = hit;
-		dustPart = dust;
-		dustTrailPart = dustTrail;
-		recoilPart = recoil;
-
-		acidPart = acidA;
-		emberPart = emberA;
-		exploPart = exploA;
-		megaDrainPart = drainA;
-		icePart = iceA;
-		quickAtkPart = quickA;
-		rockPart = rockA;
-		swiftPart = swiftA;
-		thunderPart = thunderA;
-		toxicPart = toxicA;
-		waterPart = wtrA;
-
-		acidHit = acidH;
-		emberHit = emberH;
-		exploHit = exploH;
-		megaDrainHit = drainH;
-		iceHit = iceH;
-		rockHit = rockH;
-		swiftHit = swiftH;
-		thunderHit = thunderH;
-		toxicHit = toxicH;
-		waterHit = wtrH;
-		*/
-
-
-	}
 
 	void Awake() {
 		me = this;
@@ -114,6 +18,5 @@ public class AtkFXMain : MonoBehaviour {
 		ParticleSystem PSClone;
 		PSClone = Instantiate (PSname, location, rotation);
 		PSClone.Play ();
-		//Destroy (PSClone);
 	}
 }
--- Assets/Scripts/FX/SoundFX.cs
+++ Assets/Scripts/FX/SoundFX.cs
@@ -2,6 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//TO USE THIS SCRIPT (to play sounds), type the line of code below
+//SoundFX.me.PlaySound(clipName, vol); //put this line anywhere you want in your code
+
+
 public class SoundFX : MonoBehaviour {
 
 	//accessible by all scripts
@@ -12,60 +16,12 @@ public class SoundFX : MonoBehaviour {
 	public AudioSource[] audSources;
 	public int numAudSources; //number of audio sources needed -- if unsure of exact amount, more = better (the extras will remain blank)
 
-	/* not working
-	//audioclips accessible to all scripts
-	public static AudioClip champion;
-	public static AudioClip changing;
-	public static AudioClip fireworks;
-	public static AudioClip goodHit;
-	public static AudioClip heavyBlows;
-	public static AudioClip intenseBattle;
-	public static AudioClip OhGolem;
-	public static AudioClip OhJolteon;
-	public static AudioClip OhLapras;
-	public static AudioClip OhRapidash;
-	public static AudioClip OhSnorlax;
-	public static AudioClip OhVileplume;
-	public static AudioClip UpInTheAir;
-
-	//defining specific audioclips in the editor
-	public AudioClip championAS;
-	public AudioClip changingAS;
-	public AudioClip fireworksAS;
-	public AudioClip goodHitAS;
-	public AudioClip heavyBlowsAS;
-	public AudioClip intenseBattleAS;
-	public AudioClip OhGolemAS;
-	public AudioClip OhJolteonAS;
-	public AudioClip OhLaprasAS;
-	public AudioClip OhRapidashAS;
-	public AudioClip OhSnorlaxAS;
-	public AudioClip OhVileplumeAS;
-	public AudioClip UpInTheAirAS;
-*/
 
 	void Awake() {
 		me = this;
 	}
 
 	void Start() {
-		/*
-		//set the defined audioclips = to the accessible audioclips
-		champion = championAS;
-		changing = changingAS;
-		fireworks = fireworksAS;
-		goodHit = goodHitAS;
-		heavyBlows = heavyBlowsAS;
-		intenseBattle = intenseBattleAS;
-		OhGolem = OhGolemAS;
-		OhJolteon = OhJolteonAS;
-		OhLapras = OhLaprasAS;
-		OhRapidash = OhRapidashAS;
-		OhSnorlax = OhSnorlaxAS;
-		OhVileplume = OhVileplumeAS;
-		UpInTheAir = UpInTheAirAS;
-		*/
-
 		//creating new audiosources in the list
 		audSources = new AudioSource[numAudSources]; //if unsure of exact amount, more = better (the extras will remain blank)
 		for (int i = 0; i < audSources.Length; i++) {
@@ -76,7 +32,6 @@ public class SoundFX : MonoBehaviour {
 
 
 	//public function to call when you want to play a sound
-		//SoundFX.me.PlaySound(clipName, vol); //put this code anywhere you want in your code
 	public void PlaySound(AudioClip clipName, float vol) {
 		int sNum = GetSourceNum ();
 		audSources [sNum].clip = clipName;
